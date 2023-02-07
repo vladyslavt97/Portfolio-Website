393f07d3d4287dbc850b0c5b072c5dae6657185d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-07 18:02:01 +0100

    Color black

--- pages/contacts/Contacts.tsx
+++ pages/contacts/Contacts.tsx
@@ -5,7 +5,7 @@ export default function Contacts() {
     <div className='flex flex-col items-center text-center' id='contacts'>
         <h3 className='text-2xl'>Get in Touch</h3>
         <div className='p-3'>
-            <h5 className='p-1'>vladyslavt97@gmail.com</h5>
+            <h5 className='p-1 text-black'>vladyslavt97@gmail.com</h5>
             <h5 className='p-1'>+491621616242, +380950174826</h5>
         </div>
     </div>
