d54122c835daba04ba06599086cb93d4b38dbc46
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-09 10:15:18 +0100

    Sending email with timeout and two messages

--- pages/contacts/sentemailtome/SendEmailToMe.tsx
+++ pages/contacts/sentemailtome/SendEmailToMe.tsx
@@ -5,6 +5,9 @@ export default function SendEmailToMe(){
     const [userName, setUserName] = useState<string>('');
     const [emailState, setEmailState] = useState<string>('');
     const [messageState, setMessageState] = useState<string>('');
+    const [sending, setSending] = useState<boolean>()
+    const [sent, setSent] = useState<boolean>()
+    const form = useRef<HTMLFormElement>(null);
 
     const handleChangeUserName = (event: ChangeEvent<HTMLInputElement>) => {
         setUserName(event.target.value);
@@ -15,8 +18,8 @@ export default function SendEmailToMe(){
     const handleChangeMessage = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMessageState(event.target.value);
     }
-    const form = useRef<HTMLFormElement>(null);
     const sendEmail = (e: FormEvent<HTMLFormElement>) => {
+        setSending(true);
         e.preventDefault();
         if(form.current && process.env.NEXT_PUBLIC_YOUR_SERVICE_ID && process.env.NEXT_PUBLIC_YOUR_TEMPLATE_ID && process.env.NEXT_PUBLIC_YOUR_PUBLIC_KEY) {
             emailjs
@@ -32,6 +35,12 @@ export default function SendEmailToMe(){
                 setUserName('')
                 setEmailState('')
                 setMessageState('')
+                setSent(true)
+                setTimeout(() => {
+                  console.log("Delayed for 2 second.");
+                  setSent(false);
+                  setSending(false);
+                }, 2000)
                 },
                 (error) => {
                 console.log(error.text);
@@ -40,34 +49,45 @@ export default function SendEmailToMe(){
         }
     };
 
-  return (
-    <form ref={form} onSubmit={sendEmail} 
-    className=" bg-gradient-to-r from-cyan-500 to-teal-500 flex justify-center mt-10 mx-0 flex-col items-center rounded-2xl"
-    id="sendemail">
-      <label className=" text-lg text-black mt-5">Name</label>
-      <br />
-      <input className="rounded-lg" 
-        type="text" name="user_name" 
-        onChange={handleChangeUserName}
-        value={userName}/>
-      <br />
-      <label className=" text-lg text-black">Email</label>
-      <br />
-      <input className="rounded-lg"  
-        type="email" name="email" 
-        onChange={handleChangeEmail}
-        value={emailState}/>
-      <br />
-      <label className=" text-lg text-black">Message</label>
-      <br />
-      <textarea className="rounded-lg"  
-        name="message" 
-        onChange={handleChangeMessage}
-        value={messageState}
-        />
-      <br />
-      <input type="submit" value="Send" 
-      className=" cursor-pointer bg-white px-8 py4 mb-10 rounded-lg"/>
-    </form>
-  );
+  return <div>
+        {sent && <div 
+        className=" bg-gradient-to-r from-cyan-500 to-teal-500 flex justify-center mt-10 mx-0 flex-col items-center rounded-tl-2xl rounded-tr-2xl" id="sending">
+            <h2 className=" text-2xl text-black">Sent 🎉</h2>
+          </div>
+        }
+        {sending && <div 
+        className=" bg-gradient-to-r from-cyan-500 to-teal-500 flex justify-center mt-10 mx-0 flex-col items-center rounded-tl-2xl rounded-tr-2xl" id="sending">
+            <h2 className=" text-2xl text-black">Sending...</h2>
+          </div>
+        }
+        {!sending &&
+        <form ref={form} onSubmit={sendEmail} 
+        className=" bg-gradient-to-r from-cyan-500 to-teal-500 flex justify-center mt-10 mx-0 flex-col items-center rounded-tl-2xl rounded-tr-2xl"
+        id="sendemail">
+          <label className=" text-lg text-black mt-5">Name</label>
+          <br />
+          <input className="rounded-lg bg-white" 
+            type="text" name="user_name" 
+            onChange={handleChangeUserName}
+            value={userName}/>
+          <br />
+          <label className=" text-lg text-black">Email</label>
+          <br />
+          <input className="rounded-lg bg-white"  
+            type="email" name="email" 
+            onChange={handleChangeEmail}
+            value={emailState}/>
+          <br />
+          <label className=" text-lg text-black">Message</label>
+          <br />
+          <textarea className="rounded-lg bg-white"  
+            name="message" 
+            onChange={handleChangeMessage}
+            value={messageState}
+            />
+          <br />
+          <input type="submit" value="Send" 
+          className=" cursor-pointer bg-white px-8 py4 mb-10 rounded-lg"/>
+        </form>}
+      </div>
 };
