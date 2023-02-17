49b4a52db0b76aac85ba3199a86cf47199fe1e54
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-17 10:58:08 +0100

    CV changed (link and png)

--- pages/resume/Resume.tsx
+++ pages/resume/Resume.tsx
@@ -7,7 +7,7 @@ export default function Resume() {
   return <div>
           <h2 className='text-center mt-20 text-3xl text-black' id='resume'>Resume</h2>
           <div className="flex justify-center flex-col my-5">
-            <a href="https://drive.google.com/file/d/15_7ajFz_EILhek6YyGEEWw448-bsB03C/view?usp=share_link" 
+            <a href="https://drive.google.com/file/d/1JD_OtunqnWTPIblIqNl9xPvxcwk32q-c/view?usp=sharing" 
             target="_blank" 
             rel="noopener noreferrer"
             className="w-60 mx-auto mt-5 bg-teal-500 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded text-center"
