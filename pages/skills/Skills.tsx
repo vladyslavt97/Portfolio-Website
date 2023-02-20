1227e13a4497d6893ffe523885957fd24ef9a4cc
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-20 14:31:48 +0100

    Before implementing animated menu

--- pages/skills/Skills.tsx
+++ pages/skills/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import codewars from '../../public/codewars.png'
+import { motion } from "framer-motion"
 
 export default function Skills() {
   return (
@@ -12,19 +13,189 @@ export default function Skills() {
         </p>
         
         <div className='flex flex-row justify-center bg-transparent' id='ticker'>
-          <Image id="img1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="some" width={50} height={50}/>
-          <Image id="img2" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img3" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="some" width={50} height={50}/>
-          <Image id="img4" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img5" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img6" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/socketio/socketio-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img7" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="some" width={50} height={50}/>
-          <Image id="img8" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img9" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img10" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="some" width={50} height={50}/> 
-          <Image id="img11" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg" alt="some" width={50} height={50}/>
-          <Image id="img12" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/handlebars/handlebars-original-wordmark.svg" alt="some" width={80} height={80}/>
-          <Image id="img13" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="some" width={100} height={100}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "45deg",
+            }}
+            transition={{
+                duration: 0.9,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "-25deg",
+            }}
+            transition={{
+                duration: 1.1,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "-15deg",
+            }}
+            transition={{
+                duration: 1.3,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "35deg",
+            }}
+            transition={{
+                duration: 1.5,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "35deg",
+            }}
+            transition={{
+                duration: 1.7,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "25deg",
+            }}
+            transition={{
+                duration: 1.9,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/socketio/socketio-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "5deg",
+            }}
+            transition={{
+                duration: 2.1,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "45deg",
+            }}
+            transition={{
+                duration: 2.3,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "-10deg",
+            }}
+            transition={{
+                duration: 2.5,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" alt="some" width={50} height={50}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "-85deg",
+            }}
+            transition={{
+                duration: 2.7,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="some" width={50} height={50}/> 
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "55deg",
+            }}
+            transition={{
+                duration: 2.9,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg" alt="some" width={50} height={50}/>
+         <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "5deg",
+            }}
+            transition={{
+                duration: 3.1,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/handlebars/handlebars-original-wordmark.svg" alt="some" width={80} height={80}/>
+          <motion.img 
+            initial={{
+                opacity: 0,
+                rotate: "-36deg",
+            }}
+            transition={{
+                duration: 3.3,
+            }}
+            whileInView={{
+                opacity: 1,
+                rotate: "0deg",
+            }}
+            viewport={{once: true}}
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="some" width={100} height={100}/>
         </div>
         <div className='flex flex-row justify-center items-center gap-4 py-9'>
           <h4 className='text-black'>My page on Codewars: </h4>
