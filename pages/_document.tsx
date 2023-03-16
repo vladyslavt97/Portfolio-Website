8115163872822917b7cec011c17b469583b0b44d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-17 00:19:34 +0100

    add bg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Image from 'next/image'
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        {/* add bg */}
+        {/* <Image className='absolute z-[0]' src="/telegram.jpg" alt="bg" width={100} height={100}/> */}
         <Main />
         <NextScript />
       </body>
