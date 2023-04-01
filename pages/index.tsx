e1af9437c80d4f78e71549bcdde8bc7a11a67cdd
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-01 17:57:14 +0200

    Testing color bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import { useState } from 'react'
 import Resume from '../components/resume/Resume'
 import Projects from '../components/projects/Projects'
 import Skills from '../components/skills/Skills'
@@ -13,16 +11,31 @@ import { useSelector} from "react-redux";
 import { RootState} from "../components/redux/store";
 import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const colors = ["bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-pink-300 via-purple-300 to-indigo-400", "bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500", "bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-orange-300 via-lime-200 to-emerald-200"]
+
 
 export default function Home() {
   const darkmode = useSelector((state: RootState) => state.darkMode.darkModeVal);
   console.log('Welcome to my portfolio website!🎉')
   const isOpen = useSelector((state: RootState) => state.openerState.openValue);
-  
-
   const bgColorRedux = useSelector((state: RootState) => state.bgColor.bgColorValue);
     console.log(bgColorRedux);
+
+  // const [color, setColor] = useState(bgColorRedux);
+  // useEffect(()=>{
+  //   console.log('here');
+    
+  //   if(bgColorRedux === 1){
+  //     setColor(colors[1])
+  //   } else if (bgColorRedux === 2){
+  //     setColor(colors[0])
+  //   }
+
+  // }, [bgColorRedux])
+  // console.log(color);
+// console.log('color', color);
+
+  
   return (
     <div className={darkmode ? 'dark' : ''}>
       <Head>
@@ -33,7 +46,7 @@ export default function Home() {
       </Head>
 
       <main className={isOpen ? ' bg-tele bg-no-repeat bg-cover md:bg-gradient-to-tr from-yellow-200/50 to-lime-500/80 dark:bg-gray-600 dark:bg-none overflow-hidden h-[100vh]' 
-                          : ' bg-tele bg-no-repeat bg-cover md:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-orange-300 via-lime-200 to-emerald-200 dark:bg-gray-600 dark:bg-none  overflow-scroll h-[100vh]'}>
+                          : ` bg-tele bg-no-repeat bg-cover md:${colors[bgColorRedux-1]} dark:bg-gray-600 dark:bg-none  overflow-scroll h-[100vh]`}>
         <div id='start'>
           <Navigation/>
         </div>
