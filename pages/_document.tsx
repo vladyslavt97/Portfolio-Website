bfbef87708a4157df21ca03ad27aa8f6cd2c3618
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-21 18:52:02 +0200

    dont changed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Add the link to the font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
