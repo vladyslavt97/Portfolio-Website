d21f445bbf8a0e65e03cc2bbe227840f98d884e1
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-04-02 23:51:12 +0200

    starting to work on a ticker

---
+++ components/Ticker.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import Skills from "skills.json";
+
+function MyComponent() {
+    console.log(Skills);
+    const [newArray, setNewArray] = useState(Skills);
+    const [screenWidth, setScreenWidth] = useState<number | null>(null);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+
+        // Set initial width
+        setScreenWidth(window.innerWidth);
+
+        // Event listener for window resize
+        window.addEventListener("resize", handleResize);
+
+        // Cleanup function to remove event listener
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    //
+    const targetRef = useRef(null);
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                // Update isVisible state based on whether the target element is intersecting with the viewport
+                setIsVisible(entry.isIntersecting);
+            },
+            {
+                // Set root to null to observe the viewport
+                root: null,
+                // Set a threshold to define when the element should be considered as intersecting
+                threshold: 0.5, // 0.5 means when 50% of the target is visible
+            }
+        );
+
+        // Start observing the target element
+        if (targetRef.current) {
+            observer.observe(targetRef.current);
+        }
+
+        // Cleanup function to disconnect the observer when component unmounts
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+    console.log(isVisible);
+
+    return (
+        <div>
+            Screen Width: {screenWidth}
+            <div ref={targetRef}>
+                {/* Render your content here */}
+                {isVisible ? "In viewport" : "Out of viewport"}
+            </div>
+            <div className="moving-element flex flex-row">
+                {newArray.map((n: any, i: any) => (
+                    <div key={i}>
+                        <Image src={n.src} alt="some" width={50} height={50} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default MyComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ import { useSelector} from "react-redux";
 import { RootState} from "../components/redux/store";
 import Link from 'next/link'
 import { useEffect } from 'react'
+import MyComponent from '@/components/Ticker'
 
 const colors = ["bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-pink-300 via-purple-300 to-indigo-400", "bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500", "bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-orange-300 via-lime-200 to-emerald-200"]
 
@@ -50,6 +51,7 @@ export default function Home() {
         <Contacts />
 
         <section>
+          <MyComponent/>
           <Skills/>
           <Projects />
           <Collaboration/>
