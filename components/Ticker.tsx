9fc115d667335e1e2a2126c9623f95e35876f31b
Vladyslav <vladyslav.tsurkanenko@traxion.com>
2024-04-15 23:09:55 +0200

    ticker is working

--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -1,86 +1,26 @@
-import { useEffect, useRef, useState } from "react";
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+import Skills from "skills.json"
 import Image from "next/image";
-import Skills from "skills.json";
 
-function MyComponent() {
-    const [newArray, setNewArray] = useState(Skills);
-    const [outOfViewportIndexes, setOutOfViewportIndexes] = useState<number[]>(
-        []
-    );
-    const [screenWidth, setScreenWidth] = useState<number | null>(null);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setScreenWidth(window.innerWidth);
-        };
-
-        // Set initial width
-        setScreenWidth(window.innerWidth);
-
-        // Event listener for window resize
-        window.addEventListener("resize", handleResize);
-
-        // Cleanup function to remove event listener
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    const targetRefs = useRef<Array<HTMLDivElement | null>>(
-        newArray.map(() => null)
-    );
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                const outOfViewportIndexes: number[] = [];
-                entries.forEach((entry, index) => {
-                    if (!entry.isIntersecting) {
-                        outOfViewportIndexes.push(index);
-                    }
-                });
-                setOutOfViewportIndexes(outOfViewportIndexes);
-            },
-            {
-                // Set root to null to observe the viewport
-                root: null,
-                // Set a threshold to define when the element should be considered as intersecting
-                threshold: 0.5, // 0.5 means when 50% of the target is visible
-            }
-        );
-
-        // Start observing the target elements
-        targetRefs.current.forEach((targetRef) => {
-            if (targetRef) {
-                observer.observe(targetRef);
-            }
-        });
-
-        // Cleanup function to disconnect the observer when component unmounts
-        return () => {
-            observer.disconnect();
-        };
-    }, [newArray]);
-
-    return (
-        <div>
-            <div className="flex flex-row w-full">
-                {newArray.map((n: any, i: number) => (
-                    <div
-                        key={i}
-                        className="moving-element"
-                        ref={(el) => (targetRefs.current[i] = el)}
-                    >
-                        <Image src={n.src} alt="some" width={50} height={50} />
-                    </div>
-                ))}
-            </div>
-            <div>
-                <p>
-                    Indexes of elements out of viewport:{" "}
-                    {outOfViewportIndexes.join(", ")}
-                </p>
-            </div>
+const Ticker = () => {
+    
+  return (
+    <div className='mt-[200px]'>
+    <Marquee autoFill speed={50}>
+      {Skills.map((item, index) => (
+        <div key={index} className='mr-2 text-white w-[200px]'>
+            <Image
+                src={item.src}
+                alt={item.src}
+                width={item.width}
+                height={item.height}
+            />
         </div>
-    );
-}
+      ))}
+    </Marquee>
+    </div>
+  );
+};
 
-export default MyComponent;
+export default Ticker;
