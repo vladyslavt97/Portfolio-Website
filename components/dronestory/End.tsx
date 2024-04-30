7cc8abe18d6dee9bb42cc0c80350134c45f118a3
vladyslavt97 <vladyslavt97@gmail.com>
2024-04-30 11:43:21 +0200

    end

--- components/dronestory/End.tsx
+++ components/dronestory/End.tsx
@@ -7,7 +7,7 @@ export default function End({}: Props) {
   return (
     <div className=' md:p-52'>
         Thank you for listening!
-        <Image src="/drone/end.jpg" alt='brian' width={200} height={200} className='w-[100px] rounded-full'/>
+        <Image src="/drone/end.jpg" alt='brian' width={2000} height={2000} className='w-[full]'/>
         
         <div className='flex flex-col justify-end items-end'>
             <span className='italic text-lg'>If you have any questions, please please do NOT ask them!</span>
