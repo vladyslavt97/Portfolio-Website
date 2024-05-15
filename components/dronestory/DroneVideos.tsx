d8944ad169a166087d080ad7e7341e0b1928d094
vladyslavt97 <vladyslavt97@gmail.com>
2024-05-15 14:56:59 +0200

    new video Uz

--- components/dronestory/DroneVideos.tsx
+++ components/dronestory/DroneVideos.tsx
@@ -1,28 +1,40 @@
-import YouTube, { YouTubeProps } from 'react-youtube';
+import YouTube, { YouTubeProps } from "react-youtube";
 
-type Props = {}
+type Props = {};
 
 export default function DroneVideos({}: Props) {
-    const opts: YouTubeProps['opts'] = {
-        height: '150',
-        width: '300',
-        playerVars: {
-          // https://developers.google.com/youtube/player_parameters
-          autoplay: 0,
-        },
-      };
+  const opts: YouTubeProps["opts"] = {
+    height: "150",
+    width: "300",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
 
-    const videos = ["lG-A-RCj7Yw", "acDfNaPJsqQ",  "NrKJctIlMzQ",  "czJQWZ2sAps", "vU2FuQGVuRs", "b0g387reMOM", "_ZJrzzf4loE", "gmh6rXdEkqg", "hJU8RS2mKC4", "rBNabM-yGAY", "8Ng_maGBAng", "iWYv1Z6o02A"]
-
+  const videos = [
+    "HvMQPASkNWw",
+    "lG-A-RCj7Yw",
+    "acDfNaPJsqQ",
+    "NrKJctIlMzQ",
+    "czJQWZ2sAps",
+    "vU2FuQGVuRs",
+    "b0g387reMOM",
+    "_ZJrzzf4loE",
+    "gmh6rXdEkqg",
+    "hJU8RS2mKC4",
+    "rBNabM-yGAY",
+    "8Ng_maGBAng",
+    "iWYv1Z6o02A",
+  ];
 
   return (
-    <div className='flex flex-row flex-wrap gap-3 justify-center items-center w-full mt-2 mb-20'>
-        {videos.map((e:any, i:any) =>(
-            <div key={i}>
-              <YouTube opts={opts} videoId={e} />
-            </div>
-        ))}
-
+    <div className="flex flex-row flex-wrap gap-3 justify-center items-center w-full mt-2 mb-20">
+      {videos.map((e: any, i: any) => (
+        <div key={i}>
+          <YouTube opts={opts} videoId={e} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
