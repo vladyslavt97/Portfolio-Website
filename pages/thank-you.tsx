b772b61be961e9d8a0cff3f04aa757e3d16ee38b
vladyslavt97 <vladyslavt97@gmail.com>
2024-07-01 23:57:34 +0200

    thank you page

--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -1,13 +1,21 @@
+"use client";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 
 type Props = {};
 
 export default function ThankYou({}: Props) {
-  const route = useRouter();
-  setTimeout(() => {
-    route.push("/");
-  }, 3000);
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 3000);
+
+    // Clean up the timer if the component unmounts before the timeout is complete
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <div className="flex justify-center items-center h-screen">
       <h1>Thank you for your contribution!</h1>
