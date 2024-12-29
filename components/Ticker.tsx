9b529b001233ac4adcf4f09e4ddbc21139a27de0
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-12-29 21:51:17 +0100

    ticker spacing

--- components/Ticker.tsx
+++ components/Ticker.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Ticker = () => {
     return (
-        <Marquee autoFill speed={30} className="fadeouttrick">
+        <Marquee autoFill speed={30} className="fadeouttrick relative -top-7">
             {Skills.map((item, index) => (
                 <div key={index} className=" text-white w-16">
                     <Image
--- components/navigation/Logo.tsx
+++ components/navigation/Logo.tsx
@@ -1,12 +1,16 @@
-import Image from 'next/image'
-import VT from '../../public/VT.png'
+import Image from "next/image";
+import VT from "../../public/VT.png";
 
-type Props = {}
+type Props = {};
 
 export default function Logo({}: Props) {
-  return (
-    <div>
-        <Image alt='VT' src={VT} className="w-[120px] h-[40px] px-8 object-contain"/>
-    </div>
-  )
+    return (
+        <div>
+            <Image
+                alt="VT"
+                src={VT}
+                className="w-[120px] h-[40px] px-8 object-contain"
+            />
+        </div>
+    );
 }
