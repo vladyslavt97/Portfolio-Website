c4057072208edfa3d0becd77448db02af2a6f158
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2025-01-02 01:15:58 +0100

    fix

--- components/dronestory/Drone.tsx
+++ components/dronestory/Drone.tsx
@@ -11,79 +11,79 @@ import DroneLaws from "./DroneLaws";
 type Props = {};
 
 export default function Drone({}: Props) {
-  return (
-    <div className="overflow-y-auto h-screen bg-gradient-to-br from-blue-100 to-yellow-100">
-      <div className="relative">
-        <h1 className="text-center absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-[50px] qwe">
-          My Drone Story
-        </h1>
-        <Image
-          src="/drone/manyimages.jpg"
-          alt="manyimages"
-          width={2000}
-          height={2000}
-          className="w-screen"
-          loading="eager"
-          priority={true}
-        />
-      </div>
+    return (
+        <div className="overflow-y-auto h-screen bg-gradient-to-br from-blue-100 to-yellow-100">
+            <div className="relative">
+                <h1 className="text-center absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-[50px] qwe">
+                    My Drone Story
+                </h1>
+                <Image
+                    src="/drone/manyimages.jpg"
+                    alt="manyimages"
+                    width={1000}
+                    height={1000}
+                    className="w-screen"
+                    loading="eager"
+                    priority={true}
+                />
+            </div>
 
-      <div className="flex flex-col justify-center items-center mt-10">
-        <span className="italic">Timeline</span>
-        <div className="flex flex-row justify-around w-full">
-          <span>2020</span> <span>==={">"}</span> <span>2022</span>{" "}
-          <span>==={">"}</span> <span>2024</span>
+            <div className="flex flex-col justify-center items-center mt-10">
+                <span className="italic">Timeline</span>
+                <div className="flex flex-row justify-around w-full">
+                    <span>2020</span> <span>==={">"}</span> <span>2022</span>{" "}
+                    <span>==={">"}</span> <span>2024</span>
+                </div>
+            </div>
+            <div className="flex flex-row justify-between items-center mx-10">
+                <Image
+                    src="/drone/mini.jpg"
+                    alt="manyimages"
+                    width={300}
+                    height={300}
+                    className="rounded-2xl h-[180px]"
+                />
+                <IoClose size={50} color="lightblue" />
+                <Image
+                    src="/drone/fpv/6.jpg"
+                    alt="6"
+                    width={300}
+                    height={300}
+                    className="rounded-2xl h-[180px]"
+                />
+            </div>
+
+            <hr />
+
+            <h1 className="text-center mt-10 font-bold text-xl">Gallery</h1>
+            <Gallery />
+
+            <Image
+                src="/drone/DJI_0067-PANO.jpg"
+                alt="manyimages"
+                width={2000}
+                height={2000}
+                className="w-full"
+            />
+
+            <h1 className="text-center mt-10 font-bold text-xl">Videos</h1>
+            <DroneVideos />
+
+            <Image
+                src="/drone/IMG_6630-PANO.jpg"
+                alt="manyimages"
+                width={2000}
+                height={2000}
+                className="w-full"
+            />
+
+            <FPV />
+
+            <DroneParts />
+
+            <DroneLaws />
+
+            <End />
         </div>
-      </div>
-      <div className="flex flex-row justify-between items-center mx-10">
-        <Image
-          src="/drone/mini.jpg"
-          alt="manyimages"
-          width={300}
-          height={300}
-          className="rounded-2xl h-[180px]"
-        />
-        <IoClose size={50} color="lightblue" />
-        <Image
-          src="/drone/fpv/6.jpg"
-          alt="6"
-          width={300}
-          height={300}
-          className="rounded-2xl h-[180px]"
-        />
-      </div>
-
-      <hr />
-
-      <h1 className="text-center mt-10 font-bold text-xl">Gallery</h1>
-      <Gallery />
-
-      <Image
-        src="/drone/DJI_0067-PANO.jpg"
-        alt="manyimages"
-        width={2000}
-        height={2000}
-        className="w-full"
-      />
-
-      <h1 className="text-center mt-10 font-bold text-xl">Videos</h1>
-      <DroneVideos />
-
-      <Image
-        src="/drone/IMG_6630-PANO.jpg"
-        alt="manyimages"
-        width={2000}
-        height={2000}
-        className="w-full"
-      />
-
-      <FPV />
-
-      <DroneParts />
-
-      <DroneLaws />
-
-      <End />
-    </div>
-  );
+    );
 }
---
+++ components/violin.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { AnimatedTestimonials } from "@/components/effect/AnimatedTestimonials";
+
+type Props = {};
+
+export default function Violin({}: Props) {
+    return (
+        <div className="flex flex-col justify-start items-center w-screen bg-white px-3 overflow-y-auto h-screen">
+            <h1 className="text-xl font-bold mt-6">Gallery</h1>
+            <AnimatedTestimonials
+                testimonials={[
+                    {
+                        quote: "I play with my heart",
+                        name: "Berlin 2024",
+                        designation: "kamermusikverein",
+                        src: "/violin/vlad2.png",
+                    },
+                    {
+                        quote: "I play with my heart",
+                        name: "Berlin 2024",
+                        designation: "kamermusikverein",
+                        src: "/violin/vlad3.png",
+                    },
+                ]}
+            />
+            <h1 className="text-xl font-bold mt-12">Videos</h1>
+        </div>
+    );
+}
